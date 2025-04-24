cc989390b5006750511e90f977aee4e68cd5a52c
Natsit Buraphatsrilert <natsitdido@gmail.com>
2025-04-24 23:17:08 +0700

    Add router to signin for userinfo page

--- src/app/userinfo/page.tsx
+++ src/app/userinfo/page.tsx
@@ -3,16 +3,20 @@ import { useSession } from "next-auth/react"
 import { useEffect, useState } from "react"
 import getUserProfile from "@/libs/getUserProfile"
 import PaymentMethod from "@/components/PaymentMethod"
+import { useRouter } from "next/navigation";
 
 export default function UserInfoPage() {
   const { data: session, status } = useSession()
   const [user, setUser] = useState<any>(null)
+  const router = useRouter();
 
   useEffect(() => {
     const fetchUser = async () => {
       if (session?.user.token) {
         const data = await getUserProfile(session.user.token)
         setUser(data.data)
+      } else {
+        router.push('/api/auth/signin')
       }
     }
     fetchUser()
