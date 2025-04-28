359e10a54945d2ead4cdc196eeda7e4a5ed8ee03
thunwa05 <thunwawa48@gmail.com>
2025-04-28 18:58:51 +0700

    test playwright

---
+++ tests/payment.test.js
@@ -0,0 +1,117 @@
+import { test, expect } from '@playwright/test';
+
+test('create payment', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.getByRole('textbox', { name: 'john@example.com' }).click();
+  await page.getByRole('textbox', { name: 'john@example.com' }).fill('abc@gmail.com');
+  await page.getByRole('textbox', { name: '••••••••' }).click();
+  await page.getByRole('textbox', { name: '••••••••' }).fill('123456');
+  await page.getByRole('main').getByRole('button', { name: 'Sign In' }).click();
+  await page.getByRole('button', { name: 'abc' }).click();
+  await page.getByRole('button', { name: 'Payment Method' }).click();
+  await page.getByRole('combobox').click();
+  await page.getByRole('option', { name: 'Credit Card' }).click();
+  await page.getByRole('textbox', { name: 'Enter card number' }).click();
+  await page.getByRole('textbox', { name: 'Enter card number' }).fill('1111111111111');
+  await page.getByRole('textbox', { name: 'Enter payment name' }).click();
+  await page.getByRole('textbox', { name: 'Enter payment name' }).fill('card');
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await page.getByRole('button', { name: 'Sign Out' }).click();
+  await page.getByRole('main').getByRole('button', { name: 'Sign Out' }).click();
+});
+test('error create payment', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.getByRole('textbox', { name: 'john@example.com' }).click();
+  await page.getByRole('textbox', { name: 'john@example.com' }).fill('abc@gmail.com');
+  await page.getByRole('textbox', { name: '••••••••' }).click();
+  await page.getByRole('textbox', { name: '••••••••' }).fill('123456');
+  await page.getByRole('main').getByRole('button', { name: 'Sign In' }).click();
+  await page.getByRole('button', { name: 'abc' }).click();
+  await page.getByRole('button', { name: 'Payment Method' }).click();
+  await page.getByRole('combobox').click();
+  await page.getByRole('option', { name: 'Credit Card' }).click();
+  await page.getByRole('textbox', { name: 'Enter payment name' }).fill('c');
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('textbox', { name: 'Enter card number' }).fill('123123');
+  await page.getByRole('button', { name: 'Sign Out' }).click();
+  await page.getByRole('main').getByRole('button', { name: 'Sign Out' }).click();
+});
+
+test('update payment', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.getByRole('textbox', { name: 'john@example.com' }).click();
+  await page.getByRole('textbox', { name: 'john@example.com' }).fill('abc@gmail.com');
+  await page.getByRole('textbox', { name: '••••••••' }).click();
+  await page.getByRole('textbox', { name: '••••••••' }).fill('123456');
+  await page.getByRole('main').getByRole('button', { name: 'Sign In' }).click();
+  await page.getByRole('button', { name: 'abc' }).click();
+  await page.getByRole('button', { name: 'Payment Method' }).click();
+  await page.getByRole('button', { name: 'Manage Payment Method' }).click();
+  await page.getByRole('combobox').filter({ hasText: 'Select a payment method' }).click();
+  await page.getByRole('option', { name: 'card' }).click();
+  await page.getByRole('textbox', { name: 'Card Number' }).click();
+  await page.getByRole('textbox', { name: 'Card Number' }).fill('1234567890123');
+  await page.getByRole('button', { name: 'Update' }).click();
+  await expect(page.getByText('Payment method updated')).toBeVisible();
+  await page.getByRole('button', { name: 'Sign Out' }).click();
+  await page.getByRole('main').getByRole('button', { name: 'Sign Out' }).click();
+});
+
+test('error update payment', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.getByRole('textbox', { name: 'john@example.com' }).click();
+  await page.getByRole('textbox', { name: 'john@example.com' }).fill('abc@gmail.com');
+  await page.getByRole('textbox', { name: '••••••••' }).click();
+  await page.getByRole('textbox', { name: '••••••••' }).fill('123456');
+  await page.getByRole('main').getByRole('button', { name: 'Sign In' }).click();
+  await page.getByRole('button', { name: 'abc' }).click();
+  await page.getByRole('button', { name: 'Payment Method' }).click();
+  await page.getByRole('button', { name: 'Manage Payment Method' }).click();
+  await page.getByRole('combobox').filter({ hasText: 'Select a payment method' }).click();
+  await page.getByRole('option', { name: 'card' }).click();
+  await page.getByRole('textbox', { name: 'Card Number' }).click();
+  await page.getByRole('textbox', { name: 'Card Number' }).fill('123');
+  await page.getByRole('button', { name: 'Update' }).click();
+  await expect(page.getByText('Failed to update payment')).toBeVisible();
+  await page.getByRole('button', { name: 'Sign Out' }).click();
+  await page.getByRole('main').getByRole('button', { name: 'Sign Out' }).click();
+});
+
+test('delete payment', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.getByRole('textbox', { name: 'john@example.com' }).click();
+  await page.getByRole('textbox', { name: 'john@example.com' }).fill('abc@gmail.com');
+  await page.getByRole('textbox', { name: '••••••••' }).click();
+  await page.getByRole('textbox', { name: '••••••••' }).fill('123456');
+  await page.getByRole('main').getByRole('button', { name: 'Sign In' }).click();
+  await page.getByRole('button', { name: 'abc' }).click();
+  await page.getByRole('button', { name: 'Payment Method' }).click();
+  await page.getByRole('button', { name: 'Manage Payment Method' }).click();
+  await page.getByRole('combobox').filter({ hasText: 'Select a payment method' }).click();
+  await page.getByRole('option', { name: 'card' }).click();
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.accept().catch(() => {});
+  });
+  await page.getByRole('button', { name: 'Delete' }).click();
+  await expect(page.getByText('Payment method deleted')).toBeVisible();
+  await page.getByRole('button', { name: 'Sign Out' }).click();
+  await page.getByRole('main').getByRole('button', { name: 'Sign Out' }).click();
+});
+
+test('error delete payment', async ({ page }) => {
+  await page.goto('http://localhost:3000/userinfo');
+  await expect(page.getByText('Welcome BackSign')).toBeVisible();
+});
